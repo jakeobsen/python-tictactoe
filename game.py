243ab82cb883f8ff913eff1f770d755edcacea72
Morten Jakobsen <morten@jakeobsen.com>
2018-07-23 11:12:12 +0200

    Add code license

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Copyright 2018 Morten Jakobsen. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 class TicTacToe:
     pos = ["", 1, 2, 3, 4, 5, 6, 7, 8, 9]
